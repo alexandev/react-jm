08469546e21cc2a8873eb1a551ba0fe4c5d2521a
Alexander <alexander.morenoc@outlook.com>
2021-05-02 21:47:33 -0500

    05 propiedades

--- src/App.js
+++ src/App.js
@@ -2,30 +2,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import Componente from './components/Componente';
+import Propiedades from './components/Propiedades';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
-        <Componente msg='Hola soy un componente funcional expresado desde una prop'/>
-      </section>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<section>
+
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</section>
+
+				<section>
+					<Componente msg='Hola soy un componente funcional expresado desde una prop'/>
+					
+					<hr/>
+
+					<Propiedades 
+						cadena="Esto es una cadena de texto" 
+						numero={19} 
+						booleano={true}
+						arreglo={[1,2,3]}
+						objeto={{nombre:'Jon', apellido:'Mircha'}}
+						funcion={(num)=>num*num}
+						elementoReact={<i>Esto es un elemento React</i>}
+						componenteReact={<Componente msg="Soy un componente pasado como propiedad"/>}
+					/>
+				</section>
+			</header>
+		
+		</div>
+	);
 }
 
 export default App;
